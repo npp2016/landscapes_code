4fe8e95bcfde739c7e8568adf70d5b84c4305e22
nushiamme <nushiamme@gmail.com>
2013-10-23 19:37:43 -0400

    Basic plots, canopy cover and densitometry by site.
    tree_shrub dataset needs to be fixed. Too many blanks.
    Need to talk properly about what we want, and draw some thought-graphs.

--- Veg_structure_analysis.R
+++ Veg_structure_analysis.R
@@ -13,7 +13,27 @@ wd = "C://Users/Anusha/Documents/Dropbox/Hummingbirds/Pasantias_Patagonia_2013/F
 #wd = "/Users/sarah/Desktop/Dropbox/Hummingbirds/Pasantias_Patagonia_2013/Final_Databases_2013/Vegetation_Structure_data/"
 setwd(wd)
 
-## Read in Canopy and Ground cover .csv
+## Read in csv files
 canopy <- read.csv("CanopyAndGroundCover.csv")
+siteveg_descrip <- read.csv("SiteVegetationDescriptors.csv")
+genus_key <- read.csv("GenusKey.csv")
+
+## This tree_shrub dataset is very confusing, has way too many blanks. Need to speak about how to correct this.
+## The melt() function gets very confused with the blanks.
+tree_shrub <- read.csv("TreeAndShrubSize.csv")
 
 ## Cleaning and aggregating data
+aggcanopy <- melt(data=canopy, id.vars="Site", measure.vars=c("percent_canopy_cover", 
+                    "Percent_densitometry", "percent_water_groundcover"), na.rm=T)
+## Not working! Need to fix blanks
+shrubdescrip <- melt(data=tree_shrub, id.vars="Point", measure.vars=c("Genus", "Height", "DBH_class"), na.rm=T)
+
+## Plots
+# Canopy cover by site
+cc_site <- ggplot(canopy, aes(x=Site, y=percent_canopy_cover)) + geom_boxplot()
+cc_site
+
+# Densitometry by site
+densitometry_site <- ggplot(canopy, aes(x=Site, y=Percent_densitometry)) + geom_boxplot()
+densitometry_site
+
