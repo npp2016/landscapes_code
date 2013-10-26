fda5cc305df0bf3eb5b302eac351b05dd6d658d8
nushiamme <nushiamme@gmail.com>
2013-10-26 11:55:50 -0400

    made small theme adjustments, fixed Site name issue in trees.csv dataset (in excel)

--- Veg_structure_analysis.R
+++ Veg_structure_analysis.R
@@ -8,7 +8,7 @@ library(ggplot2)
 library(reshape)
 
 ##### CHOOSE WORKING DIRECTORY (uncomment the one you like)
-## There are four separate .csv files in this folder
+## There are five separate .csv files in this folder
 wd = "C://Users/Anusha/Documents/Dropbox/Hummingbirds/Pasantias_Patagonia_2013/Final_Databases_2013/Excel_CSV_versions/Vegetation_Structure_data/"
 #wd = "/Users/sarah/Desktop/Dropbox/Hummingbirds/Pasantias_Patagonia_2013/Final_Databases_2013/Excel_CSV_versions/Vegetation_Structure_data/"
 setwd(wd)
@@ -20,33 +20,44 @@ genus_key <- read.csv("GenusKey.csv")
 trees <- read.csv("trees.csv")
 shrubs <- read.csv("shrubs.csv")
 
-## Cleaning and aggregating data
+##-------- Cleaning and aggregating data
+
+# melt canopy data using Site as id.vars
 m_canopy <- melt(data=canopy, id.vars="Site", measure.vars=c("percent_canopy_cover", 
-                   "percent_subcanopy_cover", "percent_branches_without_leaves"), na.rm=T)
+        "Percent_densitometry", "percent_subcanopy_cover", 
+        "percent_branches_without_leaves"), na.rm=T)
 
-## FIXED. Sarah- the height column was not named right. fixed now.
-treedescrip <- melt(data=trees, id.vars="Point", measure.vars=c("Genus", "Height_m", "DBH_class", "Canopy_radius_m"), na.rm=T)
+# FIXED. @ Sarah- the height column was not named right. fixed now.
+m_trees <- melt(data=trees, id.vars="Point", 
+                    measure.vars=c("Genus", "Height_m", "DBH_class", "Canopy_radius_m"), na.rm=T)
 
-## Plots
-# Canopy cover by site
-cc_site <- ggplot(m_canopy, aes(x=Site, fill=variable)) + geom_bar()
+# melt shrubs data
+m_shrubs <- melt(shrubs, id.vars="Site", measure.vars=c("Genus", "shrubs_0to0.5m", 
+                     "shrubs_0.5to1m", "shrubs_1to2m", "shrubs_2to3m", "shrubs_3plus_m"), na.rm=T)
+
+##--------- Plots
+
+# Canopy cover by site - useful
+cc_site <- ggplot(m_canopy, aes(x=Site, fill=variable)) + geom_bar() + theme_bw
 cc_site
 
 # Densitometry by site
-densitometry_site <- ggplot(canopy, aes(x=Site, y=Percent_densitometry)) + geom_boxplot()
+densitometry_site <- ggplot(canopy, aes(x=Site, y=Percent_densitometry)) + geom_boxplot() + theme_bw()
 densitometry_site
 
 # Plot tree height by genus
-tree_genus_height <- ggplot(tree, aes(x=Genus, y=Height_m)) + geom_boxplot() + 
+tree_genus_height <- ggplot(trees, aes(x=Genus, y=Height_m)) + geom_boxplot() + theme_bw() +
                               theme(axis.text.x=element_text(angle=90, vjust=0))
 tree_genus_height
 
 # Plot canopy radius by genus
-tree_genus_canopy <-  ggplot(tree, aes(x=Genus, y=Canopy_radius_m)) + geom_boxplot() +
-                              theme(axis.text.x=element_text(angle=90, vjust=0))
+tree_genus_canopy <-  ggplot(trees, aes(x=Genus, y=Canopy_radius_m)) + geom_boxplot() +
+                    theme_bw() + theme(axis.text.x=element_text(angle=90, vjust=0))
 tree_genus_canopy
 
 # Plot dbh class by genus
-tree_genus_dbh <- ggplot(tree, aes(x=Genus, y=DBH_class)) + geom_point() + coord_flip() + facet_grid(~Site)
+tree_genus_dbh <- ggplot(trees, aes(x=Genus, y=DBH_class)) + geom_point() + theme_bw() +
+  coord_flip() + facet_grid(~Site)
 tree_genus_dbh
+
 
