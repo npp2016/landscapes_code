96bcbcc0c5b15a3a45dd425352b9de9baee1a913
sarahsupp <sarah@weecology.org>
2013-11-26 11:22:07 -0500

    made notes for what to plot in phenology.r

--- phenology.r
+++ phenology.r
@@ -13,6 +13,23 @@ setwd(wd)
 
 ## Read in csv files
 pheno = read.csv("PhenologyData.csv", header = T)
+floral = read.csv("FloralCensusData.csv", header = T)
+nectar = read.csv("StandingCropData.csv", header = T)
 
+#TODO
+#convert date to julian day
+#replace plant species with species code? (easier to link between tables in the database w/o using regex)
+#slice data may not be great (slice method was deemed not to work well)
 #plot proportion of plant that is buds/flowers/fruits
+#by species?
 #by date?
+#by site?
+
+#plot nectar by plant species, site, and date, time of day?
+#need to go into data excel and make sure time shows up as time only #FIXME
+#convert date to julian day
+#volume (calculated from LengthNectar)
+#calories
+#NOTE: Volume, molarity, calories and LengthNectar are all related measurements. 
+#      Probably most interesting to plot Volume and Calories
+
