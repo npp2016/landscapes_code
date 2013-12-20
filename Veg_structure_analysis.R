f5d3e89fa02348b6a59834b3bc7c2561a407e40f
nushiamme <nushiamme@gmail.com>
2013-12-20 15:07:31 -0500

    Commented out code to separate canopy and ground. Useful for later. Changed column names in csv (CamelCasing), so changed code to match

--- Veg_structure_analysis.R
+++ Veg_structure_analysis.R
@@ -35,55 +35,55 @@ names(shrubs) <- c("Date", "Site", "Transect", "Point", "Observer", "Genus",
 
 canopy_df = data.frame(site = 1, point=1, numrows=1, meandensitometry =1, meancanopy=1, meansub=1, meanbranches=1)
 
-### TODO ###########
+### TODO ########### For future use?
 #dataframe with canopy cover data only
-canopy = cover[,1:13]
-canopy[,c(10:13)] = canopy[,c(10:13)]/100
-names(ground) = c("Day", "Month", "Year", "Site", "Transect", "Point", "Observer", "Direction", "Distance", 
+#canopy = cover[,1:13]
+#canopy[,c(10:13)] = canopy[,c(10:13)]/100
+#names(canopy) = c("Day", "Month", "Year", "Site", "Transect", "Point", "Observer", "Direction", "Distance", 
                                      "densitometry","canopy", "subcanopy", "barebranches")
 
 #### dataframe with ground cover data only
-ground = canopy[,c(1,2,3,4,5,6,7,8,9,14,15,16,17,18,19,20,21)]
-ground[,c(10:17)] = ground[,c(10:17)]/100
-names(ground) = c("Day", "Month", "Year", "Site", "Transect", "Point", "Observer", "Direction", "Distance", 
-                              "shrub", "forb", "grass", "soil", "rock", "organicmaterial", "water", "disturbance")
+#ground = canopy[,c(1,2,3,4,5,6,7,8,9,14,15,16,17,18,19,20,21)]
+#ground[,c(10:17)] = ground[,c(10:17)]/100
+#names(ground) = c("Day", "Month", "Year", "Site", "Transect", "Point", "Observer", "Direction", "Distance", 
+#                              "shrub", "forb", "grass", "soil", "rock", "organicmaterial", "water", "disturbance")
 
+## To just pull transect sites and leave out  restoration crosses
+#sites=c("HC", "PL/SC")
+#points=sort(unique(canopy$Point))
+#i=1
 
-sites=c("HC", "PL/SC")
-points=sort(unique(canopy$Point))
-i=1
-
-for (s in 1:length(sites)){
-  for (p in 1:length(points)){
-    data = subset(canopy, Site == sites[s] & Point == points[p])
-    if (nrow(data) >0) {
-      meandens = mean(data$densitometry)
-      meancanopy = mean(data$canopy)
-      meansub = mean(data$subcanopy)
-      meanbranches = mean(data$barebranches)
-      canopy_df[i,] <- c(sites[s], points[p], nrow(data), meandens, meancanopy, meansub, meanbranches)
-      i = i+1
-    }
-  }
-}
+#for (s in 1:length(sites)){
+#  for (p in 1:length(points)){
+#    data = subset(canopy, Site == sites[s] & Point == points[p])
+#    if (nrow(data) >0) {
+#      meandens = mean(data$Densitometry)
+#      meancanopy = mean(data$Canopy)
+#      meansub = mean(data$Subcanopy)
+#      meanbranches = mean(data$BareBranches)
+#      canopy_df[i,] <- c(sites[s], points[p], nrow(data), meandens, meancanopy, meansub, meanbranches)
+#      i = i+1
+#    }
+#  }
+#}
                        
 #### Melt functions ####
 # melt canopy data using Site as id.vars
 m_canopy <- melt(data=canopy, id.vars=c("Site", "Point"), 
-                 measure.vars=c("canopy", "densitometry", "subcanopy", "barebranches"), na.rm=T)
+                 measure.vars=c("Canopy", "Densitometry", "Subcanopy", "BareBranches"), na.rm=T)
 
 # melt ground cover data
 m_ground <- melt(data=ground, id.vars=c("Site", "Point"), 
-                 measure.vars=c("shrub", "forb", "grass", "soil", "rock", 
-                                "organicmaterial", "water", "disturbance"), na.rm=T)
+                 measure.vars=c("Shrub", "Forb", "Grass", "Soil", "Rock", 
+                                "OrganicMaterial", "Water", "Disturbance"), na.rm=T)
 
 #reshape trees data for values per point and genus for each of the three tree variables
 m_trees <- melt(data=trees, id.vars=c("Site", "Point", "Genus"), 
-                    measure.vars=c("Height_m", "DBH_class", "Canopy_radius_m"), na.rm=T)
+                    measure.vars=c("Height", "DBHclass", "CanopyRadius"), na.rm=T)
 
 # melt shrubs data 
 m_shrubs <- melt(shrubs, id.vars=c("Site", "Genus", "Point"), 
-                 measure.vars=c("0to0.5m", "0.5to1m", "1to2m", "2to3m", "3plusm"), na.rm=T)
+                 measure.vars=c("shrubs0to0.5m", "shrubs0.5to1m", "shrubs1to2m", "shrubs2to3m", "ocotillo3plusm"), na.rm=T)
 names(m_shrubs) <- c("Site", "Genus", "Point", "size_class", "num_indivs")
 
 #aggregate shrub data by site and size class
@@ -100,7 +100,7 @@ shb <- aggregate(num_indivs ~ Site + size_class, data = m_shrubs, FUN = sum)
 ## the best way to look at it for sure.
 
 # Densitometry by site
-densitometry_site <- ggplot(canopy, aes(x=Site, y=densitometry)) + geom_boxplot() + theme_bw()
+densitometry_site <- ggplot(canopy, aes(x=Site, y=Densitometry)) + geom_boxplot() + theme_bw()
 densitometry_site
 
 # Canopy cover by site- boxplot
@@ -115,34 +115,34 @@ ground_site <- ggplot(m_ground, aes(x=variable, y=value, fill=variable)) + theme
 ground_site
 
 # Plot tree height by genus
-tree_genus_height <- ggplot(trees, aes(x=Genus, y=Height_m, fill=Genus)) + geom_boxplot() + theme_bw() + facet_grid(~Site) +
+tree_genus_height <- ggplot(trees, aes(x=Genus, y=Height, fill=Genus)) + geom_boxplot() + theme_bw() + facet_grid(~Site) +
                            theme(axis.text.x=element_text(angle=90, vjust=0))
 tree_genus_height
 
 # Plot canopy radius by genus. Not in Rmd
-tree_genus_canopy <-  ggplot(trees, aes(x=Genus, y=Canopy_radius_m)) + geom_boxplot() +
+tree_genus_canopy <-  ggplot(trees, aes(x=Genus, y=CanopyRadius)) + geom_boxplot() +
                     theme_bw() + theme(axis.text.x=element_text(angle=90, vjust=0))
 tree_genus_canopy
 
 # Plot dbh class by genus. Not in Rmd
-tree_genus_dbh <- ggplot(trees, aes(x=Genus, y=DBH_class)) + geom_point(size=3, col="red") +
+tree_genus_dbh <- ggplot(trees, aes(x=Genus, y=DBHclass)) + geom_point(size=3, col="red") +
   theme_bw() + coord_flip() + facet_grid(~Site)
 tree_genus_dbh
 
 ##### LOOKING FOR DIFFERENCES AMONG THE TWO SITES
 # Plot tree height by site
-tree_site_height <- ggplot(trees, aes(x=Site, y=Height_m, fill=Site)) + 
+tree_site_height <- ggplot(trees, aes(x=Site, y=Height, fill=Site)) + 
   geom_boxplot() + theme_bw()
 tree_site_height
 
 #Plot tree canopy radius by site
-tree_site_canopy <- ggplot(trees, aes(x=Site, y=Canopy_radius_m, fill=Site)) + 
+tree_site_canopy <- ggplot(trees, aes(x=Site, y=CanopyRadius, fill=Site)) + 
   geom_boxplot() + theme_bw()
 tree_site_canopy
 
 #Plot DBH class by site
 ## TODO Try to do relative abundance
-tree_site_dbh <- ggplot(subset(m_trees, variable=="DBH_class"), aes(Site, fill=factor(value))) +
+tree_site_dbh <- ggplot(subset(m_trees, variable=="DBHclass"), aes(Site, fill=factor(value))) +
   geom_bar(width=0.5)
 tree_site_dbh
 
